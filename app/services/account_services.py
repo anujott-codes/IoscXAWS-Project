678ba00f1c83f95313018b58d95d11cbdd378143
Akshat Talwar <uniquedimension1@gmail.com>
2026-03-28 16:07:07 +0530

    added reset password route

--- app/services/account_services.py
+++ app/services/account_services.py
@@ -2,20 +2,18 @@ import os
 from enum import Enum
 
 from pwdlib import PasswordHash
-from sqlalchemy import Column, Integer, String, Enum as SAEnum
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy import update
 
 from app.core.database import Base, get_db
-from app.model.models import DBUser, RoleEnum
+from app.model.models import DBUser, RoleEnum, ParentDetails
 
 pwd_hasher = PasswordHash.recommended()
- 
+
 
 async def changeUserPassword(db: AsyncSession, user_id: int, new_password: str):
     hashed_pw = pwd_hasher.hash(new_password)
-    
     await db.execute(
         update(DBUser)
         .where(DBUser.id == user_id)
@@ -24,3 +22,19 @@ async def changeUserPassword(db: AsyncSession, user_id: int, new_password: str):
     await db.commit()
 
 
+async def resetPasswordToDefault(db: AsyncSession, student_id: str):
+    result = await db.execute(
+        select(ParentDetails).where(ParentDetails.student_id == student_id)
+    )
+    parent = result.scalar_one_or_none()
+    if not parent:
+        raise ValueError("Parent details not found for this student")
+
+    hashed_pw = pwd_hasher.hash(parent.parent_name)
+
+    await db.execute(
+        update(DBUser)
+        .where(DBUser.username == student_id)
+        .values(password_hash=hashed_pw, password_changed=False)
+    )
+    await db.commit()
